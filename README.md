7a9b8bf70e596f56d376b013f9b6d3c64ac85f6e
Guto Brutus <guto@email.com>
2023-12-06 21:58:14 -0300

    feat: adiciona mods para nova versão
    [+] adiciona paramatrização de delay
    [+] adiciona validações de delay, ip do host e portas
    [+] refatora código para usar mensagem de ajuda a partir de variável
    [+] adiciona documentação do projeto

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# port-knocking
-Shell Scripting para enviar um port knocking para um ip e no mínimo 2 portas
+# Port Knocking
+
+Shell Script para enviar um port knocking para um ip e no mínimo 2 portas.
+
+**Pré-requisito**: necessário ter o netcat instalado (nc).
+
+## 1. Instalação
+
+Basta clonar o repositório:
+```shell
+git clone https://github.com/gutobrutus/port-knocking
+```
+
+## 2. Utilização
+
+2.1. Após clonar, acessar o diretório port-knocking:
+```shell
+cd port-knocking
+```
+
+2.2. Executar com o padrão abaixo:
+```shell
+./portknocking.sh <delay> <host> <porta1> <porta2> [<porta3> <porta4> ...]
+```
+
+**Opções**:
+- ***delay***: tempo entre o envio de um SYN e outro
+- ***host***: Host alvo
+- ***porta\<n\>***: número da porta. No mínimo duas portas.
+
+## 3. Exemplos:
+
+3.1. Executar com um delay de 2 segundos contra as portas 5225, 3565, 1000 no host 192.168.0.100:
+```shell
+./portknocking.sh 2 192.168.0.100 5225 3565 1000
+```
+
+3.2. Executar com um delay de 1 segundo contra as portas 13, 3700 no host 192.168.10.10:
+```shell
+./portknocking.sh 1 192.168.10.10 13 3700
+```
+
+## Referência
+
+[ssh-port-knocking](https://goteleport.com/blog/ssh-port-knocking/)
--- portknocking.sh
+++ portknocking.sh
@@ -5,7 +5,8 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 LIGHT_RED='\033[1;31m'
 BLUE='\033[0;34m'
-NC='\033[0m'
+CYAN='\033[0;36m'
+NC='\033[0m' # no color
 
 echo ""
 echo -e "${YELLOW}                      _/_/_/                          _/      "
@@ -23,30 +24,102 @@ echo "  _/  _/    _/    _/  _/    _/  _/        _/  _/    _/  _/    _/  _/    _/
 echo " _/    _/  _/    _/    _/_/      _/_/_/  _/    _/  _/  _/    _/    _/_/_/      "
 echo "                                                                      _/       "
 echo -e "                                                                 _/_/  ${NC}"
-echo -e "${DARK_GREY}                                                                     v0.1.0 ${NC}"
-echo -e "${GREEN}             source: https://github.com/gutobrutus/port-knocking    ${NC}"
+echo -e "${DARK_GREY}                                                                     v0.2.0 ${NC}"
+echo -e "${GREEN}         docs/source: https://github.com/gutobrutus/port-knocking    ${NC}"
 echo ""
 
-if [ "$#" -lt 3 ]; then 
-    echo -e "${BLUE}|=========================== Iniciando a execução ============================|${NC}"
-    echo -e "${RED}ERRO:${NC} falta de argumentos necessários"
+HELP="${CYAN}Modo de uso:${NC}
+    ${GREEN}$0 <delay> <host> <porta1> <porta2> [<porta3> <porta4> ...]${NC}
+${CYAN}Opções${NC}:
+    ${GREEN}- delay: tempo entre o envio de um SYN e outro
+    - host: Host alvo
+    - porta<n>: número da porta. No mínimo duas portas.${NC}
+${CYAN}Exemplo:${NC} 
+    ${GREEN}$0 2 192.168.0.10 2300 3252 2525${NC}
+"
+HEADER="${BLUE}|=========================== Iniciando a execução ============================|${NC}"
+
+valida_host_ip() {
+    local ip="$1"
+    local ip_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+
+    if [[ ! $ip =~ $ip_regex ]]; then
+    # O argumento não atende ao formato básico
+        return 1
+    fi
+
+    IFS='.' read -r -a octetos <<< "$ip"
+
+    for octeto in "${octetos[@]}"; do
+        if ! [[ "$octeto" =~ ^[0-9]+$ ]]; then
+        # Octeto não é um número
+        return 1
+        elif ((octeto < 0 || octeto > 255)); then
+        # Octeto fora do intervalo permitido
+        return 1
+        fi
+    done
+
+    # O argumento é um IP válido
+    return 0
+}
+
+valida_porta() {
+    local porta="$1"
+
+    if ! [[ "$porta" =~ ^[0-9]+$ ]]; then
+        # A porta não é um número
+        return 1
+    elif ((porta < 1 || porta > 65535)); then
+        # A porta está fora do intervalo permitido
+        return 1
+    fi
+
+    # A porta é válida
+    return 0
+}
+
+delay="$1"
+host="$2"
+portas=("${@:3}")
+
+if ! [[ "$delay" =~ ^[0-9]+$ ]]; then
+    echo -e "${HEADER}"
+    echo -e "${RED}ERRO:${NC} ${GREEN}O delay deve ser um número inteiro${NC}"
     echo ""
-    echo -e "${LIGHT_RED}Modo de uso:${NC} ${GREEN}$0 <host> <porta1> <porta2> [<porta3> <porta4> ...]${NC}"
+    echo -e "${HELP}"
     echo ""
-    echo -e "${LIGHT_RED}Exemplo:${NC} 
-          ${GREEN}$0 192.168.0.10 2300 3252 2525${NC}"
+    exit 1
+elif ! valida_host_ip "$host"; then
+    echo -e "${HEADER}"
+    echo -e "${RED}ERRO:${NC} ${GREEN}Não foi informado um IP de Host válido${NC}"
+    echo ""
+    echo -e "${HELP}"
+    echo ""
+    exit 1
+elif [ "$#" -lt 4 ]; then 
+    echo -e "${HEADER}"
+    echo -e "${RED}ERRO:${NC} ${GREEN}faltam argumentos${NC}"
+    echo ""
+    echo -e "${HELP}"
     echo ""
     exit 1
 fi
 
-host="$1"
-portas=("${@:2}")
-echo -e "${BLUE}|=========================== Iniciando a execução ============================|${NC}"
+echo -e "${HEADER}"
 for porta in "${portas[@]}"; do
-    echo "Enviando SYN connect para $host:$porta"
-    nc -z -w 1 "$host" "$porta"
+    if ! valida_porta "$porta"; then
+        echo -e "${HEADER}"
+        echo -e "${RED}ERRO:${NC} ${GREEN}a porta informada $porta é inválida!${NC}"
+        echo ""
+        echo -e "${HELP}"
+        echo ""
+        exit 1
+    fi
+    echo "Enviando segmento TCP com flag SYN -> $host:$porta"
+    nc -z -w "$delay" "$host" "$porta"
 done
 
-echo "Envio de SYN connect finalizado"
+echo "Envio de segmentos TCP com flag SYN finalizado"
 
 echo -e "${BLUE}|============================== Fim da execução ==============================|${NC}"
